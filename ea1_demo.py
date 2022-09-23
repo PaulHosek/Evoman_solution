c3d946f440e992762c8febe57b6719f73086f863
Andreea Hazu <andreeadhazu@gmail.com>
2022-09-23 13:05:40 +0200

    multiprocessing by Nathan

--- ea1_demo.py
+++ ea1_demo.py
@@ -1,11 +1,12 @@
-# imports framework
-import itertools
+# --------------------- Import Frameworks and Libraries ---------------------- #
 import sys, os
-import numpy as np
 import json
 import time
-import matplotlib.pyplot as plt
 
+import numpy as np
+import matplotlib.pyplot as plt
+import multiprocessing as mp
+from deap import tools, creator, base, algorithms
 
 if os.environ.get('EVOMAN_FAST'):
     print("\nUsing evoman_fast!!! ...vrooom\n")
@@ -16,31 +17,35 @@ else:
 
 from evoman.environment import Environment
 from demo_controller import player_controller
-from deap import tools, creator, base, algorithms
+
+# ---------------------------------- Setup ----------------------------------- #
+
+# Prevent graphics and audio rendering to speed up simulations
 os.environ["SDL_VIDEODRIVER"] = "dummy"
 os.environ["SDL_AUDIODRIVER"] = "dummy"
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
-# create experiment folder if needed
+# Create experiment folder if needed
 experiment_name = 'ea_exp'
 if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
     os.makedirs(experiment_name + '/best_results')
+    os.makedirs(experiment_name + '/plotted_results')
     os.makedirs(experiment_name + '/plots')
 
 # DEFINE VARIABLES
 # environment variables
-enemies = [1] #list of player enemies - any from 1..8
+enemies = [1] #list of player enemies - any from 1-8
 n_runs = 10 #should be 10
-gen_size = 30 #should be 100
+gen_size = 30 #should be 100?
 pop_size = 100
 n_hidden_neurons = 10
 max_budget = 500 #default is 3000
 difficulty_level = 2 #default is 1
 enemymode = "static" #default is ai
 players_life = 100
-#deap_algorithms = ['eaMuPlusLambda', 'eaMuCommaLambda', 'eaSimple']
-deap_algorithms = ['eaMuCommaLambda']
+deap_algorithms = ['eaMuPlusLambda', 'eaMuCommaLambda', 'eaSimple']
+#deap_algorithms = ['eaMuCommaLambda']
 
 # deap variables
 mating_prob = 1
@@ -48,16 +53,16 @@ crossover_rate = 0.8
 mutation = 0.2
 toolbox, log = None, None
 
-# returns environment for specific enemies
-def get_env(enemies):
-    # initializes environments for each game/enemy type with ai player using random controller, playing against static enemy
-    return Environment(experiment_name=experiment_name,
-                     enemies=enemies,
-                     player_controller=player_controller(n_hidden_neurons),
-                     enemymode="static",
-                     level=difficulty_level,
-                     speed="fastest",
-                     timeexpire=max_budget)
+# Initialise the game environment for the chosen settings
+env = Environment(experiment_name=experiment_name,
+                  enemies=enemies,
+                  player_controller=player_controller(n_hidden_neurons),
+                  enemymode="static",
+                  level=difficulty_level,
+                  speed="fastest",
+                  timeexpire=max_budget)
+
+# -------------------------------- Functions --------------------------------- #
 
 # a game simulation for environment env and game x
 def simulation(env,x):
@@ -65,7 +70,7 @@ def simulation(env,x):
     return fitness
 
 # evaluation of game
-def cust_evaluate(env,x):
+def cust_evaluate(x):
     sim = simulation(env,x)
     return (sim,)
 
@@ -73,9 +78,7 @@ def cust_evaluate(env,x):
 def evaluate_pop(env, pop):
     # Evaluate the individuals with an invalid fitness
     invalid_ind = [indiv for indiv in pop if not indiv.fitness.valid]
-    # pop_size Environment objects
-    envs = [env for i in range(len(invalid_ind))]
-    fitness = toolbox.map(toolbox.evaluate, envs, invalid_ind)
+    fitness = toolbox.map(toolbox.evaluate, invalid_ind)
     for indiv, fit in zip(pop, fitness):
         indiv.fitness.values = fit
     return pop
@@ -111,30 +114,6 @@ def write_best(individuals, file, enemy):
         for weight in individual:
             f.write(str(weight) + "\n")
 
-def init_deap():
-    global toolbox, log
-
-    # deap - creating types: Fitness, Individual and Population
-    # Fitness - tuple, we give one for single objective, 1.0 for maximizing
-    creator.create("FitnessBest", base.Fitness, weights=(1.0,))
-    # Individual class inherited from numpy.ndarray
-    creator.create('Individual', np.ndarray, fitness=creator.FitnessBest, player_life=players_life, enemy_life=players_life)
-
-    # OPERATORS & INITIALIZATIONS OF CLASSES
-    toolbox = base.Toolbox()
-    # population drawn from uniform distribution
-    toolbox.register("indices", np.random.uniform, -1, 1)
-
-    # Register functions
-    toolbox.register("evaluate", cust_evaluate)
-    toolbox.register("mate", tools.cxTwoPoint) # crossover operator
-    toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.5)
-    toolbox.register("select", tools.selTournament,tournsize=2)
-
-    # Initialize deap logbook
-    log = tools.Logbook()
-    log.header = ['enemy','run','generation','population size','fitness mean','fitness max']
-
 def plot_exp_stats(enemy, statistics, alg):
     # TO-DO:
     # Save statistics before plotting in file so we can play more with the experiments data
@@ -161,7 +140,6 @@ def create_next_generation(env, pop, alg="eaSimple"):
         # Select the next generation individuals
         offspring = toolbox.select(pop, len(pop))
         # Vary the pool of individuals
-        algorithms.eaSimple()
         offspring = algorithms.varAnd(offspring, toolbox, mating_prob, mutation)
         # Evaluate the individuals with an invalid fitness
         offspring = evaluate_pop(env, offspring)
@@ -186,14 +164,36 @@ def create_next_generation(env, pop, alg="eaSimple"):
     best = tools.HallOfFame(1, similar=np.array_equal)
     return pop, best
 
+# ----------------------------- Initialise DEAP ------------------------------ #
+
+# deap - creating types: Fitness, Individual and Population
+# Fitness - tuple, we give one for single objective, 1.0 for maximizing
+creator.create("FitnessBest", base.Fitness, weights=(1.0,))
+# Individual class inherited from numpy.ndarray
+creator.create('Individual', np.ndarray, fitness=creator.FitnessBest, player_life=players_life, enemy_life=players_life)
+
+# OPERATORS & INITIALIZATIONS OF CLASSES
+toolbox = base.Toolbox()
+# population drawn from uniform distribution
+toolbox.register("indices", np.random.uniform, -1, 1)
+
+# Register functions
+toolbox.register("evaluate", cust_evaluate)
+toolbox.register("mate", tools.cxTwoPoint) # crossover operator
+toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.5)
+toolbox.register("select", tools.selTournament,tournsize=2)
+
+# Initialize deap logbook
+log = tools.Logbook()
+log.header = ['enemy','run','generation','population size','fitness mean','fitness max']
+
+# ---------------------------------- Main ------------------------------------ #
+
 def main():
-    init_deap()
     for alg in deap_algorithms:
         print("Algorithm: %s" % alg)
         # For each of the n enemies we want to run the experiment for:
         for enemy in enemies:
-            # Get enviroment for player and prepare DEAP
-            env = get_env([enemy])
             # number of weights for multilayer network with n_hidden_neurons
             n_weights = (env.get_num_sensors() + 1) * n_hidden_neurons + (n_hidden_neurons + 1) * 5
             toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.indices, n=n_weights)
@@ -237,4 +237,9 @@ def main():
         write_stats_in_file(log,"log_stats_" + experiment_name + alg + ".txt")
 
 if __name__ == "__main__":
+    pool = mp.Pool(processes=mp.cpu_count())
+    toolbox.register('map', pool.map)
+
     main()
+
+    pool.close()

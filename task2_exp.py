b5fc17641b4725a6858cbf88f07b6eb222358dbf
Andreea Hazu <andreeadhazu@gmail.com>
2022-10-14 20:39:41 +0200

    task 2 experiments

--- task2_exp.py
+++ task2_exp.py
@@ -1,25 +1,32 @@
 import os
 import numpy as np
 
-enemies = ['3-6-7-2-4']
-mu = [150]
-lmbd = [150]
-ngen = [100]
+enemies = ['7-8', '1-3-4-6-7']
+mu = {'cma':'20', 'cma-mo':'23'}
+lmbd = {'cma':'50', 'cma-mo':'71'}
+sigma = {'cma':'0.486', 'cma-mo':'0.224'}
+#cmo: 10, 20, 0.234
+ngen = ['300']
 nrun = '10'
-strategies = ['cma', 'cma-opl', 'cma-mo']
+strategies = ['cma-mo', 'cma']
 
 cnt = 0
 for strategy in strategies:
     for enemy in enemies:
-        for m in mu:
-            for l in lmbd:
-                for n in ngen:
-                    cnt += 1
-                    print(f"Run {cnt}")
-                    print(f"Executing.. Strategy={strategy} enemies={enemy} mu={m} lambda={l} ngen={n}")
-                    os.environ['mu'] = str(m)
-                    os.environ['lambda'] = str(l)
-                    os.environ['ngen'] = str(n)
-                    os.environ['enemy'] = str(enemy)
-                    os.system("python task2.py")
+        if len(enemy) > 3:
+            mu = {'cma': '23', 'cma-mo': '30'}
+            lmbd = {'cma': '114', 'cma-mo': '130'}
+            sigma = {'cma': '0.122', 'cma-mo': '0.19'}
+        for n in ngen:
+            cnt += 1
+            print(f"Run {cnt}")
+            print(f"Executing.. Strategy={strategy} enemies={enemy} mu={mu[strategy]} lambda={lmbd[strategy]} sigma={sigma[strategy]} ngen={n}")
+            os.environ['strategy'] = strategy
+            os.environ['enemy'] = str(enemy)
+            os.environ['nrun'] = nrun
+            os.environ['ngen'] = n
+            os.environ['mu'] = mu[strategy]
+            os.environ['lambda'] = lmbd[strategy]
+            os.environ['sigma'] = sigma[strategy]
+            os.system("python task2.py")
 print(f"\n\n{cnt} experiments run")
